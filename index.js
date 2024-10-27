95fd53131f66911347b31b6c44c67fc886162966
Dean Fox <deanfoxcd@gmail.com>
2024-10-27 12:03:54 -0400

    build demo to learn Matter.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const { World, Engine, Runner, Bodies, Render, MouseConstraint, Mouse } =
+  Matter;
+const width = 800;
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: false,
+    width,
+    height,
+  },
+});
+
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
